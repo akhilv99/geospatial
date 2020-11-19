b6af7825c9d63c3f8cbbaa14f44a022a44bf6e31
akhilv99 <akhilvaranasi1999@gmail.com>
2020-11-18 16:18:38 -0800

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,14 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name = 'Akhil Varanasi'
+>>> age = 21
+>>> favorite_cars = ['McLaren', 'Porsche', 'Lamborghini']
+>>> print(name)
+Akhil Varanasi
+>>> print(age)
+21
+>>> print {favorite_cars[0}}
+SyntaxError: invalid syntax
+>>> print (favorite_cars[0])
+McLaren
+>>> 
